c35a8ea15a35306f1c9146955cff463612322b39
Alex Hughes <ahughesalex@gmail.com>
2016-10-01 06:46:38 -0700

    Adding examples from RV's builtin packages

---
+++ examples/pyhello/pyhello.py
@@ -0,0 +1,26 @@
+from rv import rvtypes, commands
+
+__version__ = 1.0
+
+
+class PyHello(rvtypes.MinorMode):
+    "A simple example that shows how to make shift-Z start/stop playback"
+
+    def togglePlayback(self, event):
+        if commands.isPlaying():
+            commands.stop()
+            print "stopped"
+        else:
+            commands.play()
+            print "playing"
+
+    def __init__(self):
+        rvtypes.MinorMode.__init__(self)
+        self.init("pyhello", 
+                  [("key-down--Z", self.togglePlayback, "Z key")], 
+                  None)
+
+
+def createMode():
+    "Required to initialize the module. RV will call this function to create your mode."
+    return PyHello()
